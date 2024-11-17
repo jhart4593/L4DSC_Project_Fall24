f257ab6b287f81657c3312a6c28b52b224f9e1ca
jhart <justin.hart@utexas.edu>
2024-11-16 21:59:12 -0600

    updated training file for SAC

--- train_SAC.py
+++ train_SAC.py
@@ -1,23 +1,29 @@
 from stable_baselines3 import SAC
-from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.monitor import Monitor
 import wandb
 from wandb.integration.sb3 import WandbCallback
 import traceback
+import math
 
-from callbacks import WandBVideoCallback 
+from callbacks import WandBVideoCallback, checkpoint_callback 
 from config import config 
 from env import AUVEnv 
 
 run = wandb.init(
-    project="AUV_env",
+    project="AUV_env_SAC",
     config=config,
     sync_tensorboard=True,
     monitor_gym=True,
     save_code=True,
 )
 
-env = AUVEnv(render_mode="human")
-# env = make_vec_env(AUVEnv,n_envs=1)
+# Save config, env, and rewards file for each training run to wandb
+wandb.save( "./config.py")
+wandb.save("./rewards.py")
+wandb.save("./env.py")
+
+env = AUVEnv()
+env = Monitor(env)
 
 model = SAC(
     config["policy_cls"],
@@ -31,16 +37,19 @@ try:
         total_timesteps=config["max_steps"],
         callback=[
             WandbCallback(
-                verbose=config["verbose"],
-                model_save_path=f"models/{run.id}",  # f"models/{run.id}"
-                model_save_freq=100,  # 100
-                gradient_save_freq=100,  # 100
+                verbose=2,
+                model_save_path=None,  # f"models/{run.id}"
+                model_save_freq=0,  # 100
+                gradient_save_freq=0,  # 100
             ),
             # WandBVideoCallback(),
+
+            checkpoint_callback
         ],
     )
-# except Exception as e:
-#     print(f"Exception: {e}")
+    save_dir = "./final_model/"
+    model.save(save_dir + "SAC_AUV")
+
 except:
     traceback.print_exc()
 run.finish()
