e25eb232c3ba96ed727b88e03763709bec4f5775
jhart <justin.hart@utexas.edu>
2024-11-17 20:32:39 -0600

    updated rewards

--- rewards.py
+++ rewards.py
@@ -42,9 +42,56 @@ def get_rewards(vehicle, depth_err, yaw_err, pitch_err, simData, beta):
     T_depth = T_op_d(e_depth, e_depth_min)
     T_yaw = T_op(e_yaw, e_yaw_min)
     T_pitch = T_op(e_pitch, e_pitch_min)
-    neg_rud_act = beta*(del_delta_v / delta_v_max)
-    neg_stern_plane_act = beta*(del_delta_h / delta_h_max)
 
+    # Penalizing rudder commands by difference in last two commands as fraction of maximum rudder command
+    # neg_rud_act = beta*(del_delta_v / delta_v_max)
+    # neg_stern_plane_act = beta*(del_delta_h / delta_h_max)
+
+    # penalizing rudder commands over threshold of +/- 25deg
+    # delta_h = simData[-1][13]
+    # delta_v = simData[-1][12]
+
+    # if abs(delta_v) >= math.radians(25):
+    #     diff_v = abs(abs(delta_v) - math.radians(25))
+    #     diff_v_deg = math.degrees(diff_v)
+    #     neg_rud_act = beta * (math.exp(-1/diff_v_deg))
+    # else:
+    #     neg_rud_act = 0
+    
+    # if abs(delta_h) >= math.radians(25):
+    #     diff_h = abs(abs(delta_h) - math.radians(25))
+    #     diff_h_deg = math.degrees(diff_h)
+    #     neg_stern_plane_act = beta * (math.exp(-1/diff_h_deg))
+    # else:
+    #     neg_stern_plane_act = 0
+
+    # Penalizing if greatest difference in last 5 rudder commands is over threshold +/- 15deg
+    rud_cmd_lst = simData[:,12]
+    splane_cmd_lst = simData[:,13]
+    lg_r = len(rud_cmd_lst)
+    lg_s = len(splane_cmd_lst)
+    if lg_r < 6:
+        end_r = rud_cmd_lst
+        end_s = splane_cmd_lst
+    else:
+        end_r = rud_cmd_lst[lg_r-5:lg_r]
+        end_s = splane_cmd_lst[lg_s-5:lg_s]
+
+    diff_r = abs(max(end_r) - min(end_r))
+    diff_s = abs(max(end_s) - min(end_s))
+
+    if diff_r >= math.radians(15):
+        neg_rud_act = beta * math.exp(-1/math.degrees(diff_r))
+    else:
+        neg_rud_act = 0
+
+    if diff_s >= math.radians(15):
+        neg_stern_plane_act = beta * math.exp(-1/math.degrees(diff_s))
+    else:
+        neg_stern_plane_act = 0
+
+
+    # Output individual reward terms as well as sum
     indiv_terms = [T_depth,T_yaw,T_pitch,neg_rud_act,neg_stern_plane_act]
 
     r_t = (T_depth + T_yaw + T_pitch + neg_rud_act + neg_stern_plane_act)
